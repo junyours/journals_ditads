071bc98b84695602c72a65ad217287108b3de5ae
Al019 <occ.gaid.alchristian@gmail.com>
2025-04-30 11:12:04 +0800

    .

--- resources/js/Pages/Web/Magazine/AimScope.jsx
+++ resources/js/Pages/Web/Magazine/AimScope.jsx
@@ -1,5 +1,28 @@
 export default function AimScope() {
-  return (
-    <div>AimScope</div>
-  )
+    return (
+        <div className="text-justify">
+            <p>
+                zDitads. Magazine: Mindset | Mastery | Momentum is the
+                international, multidisciplinary publication of the DIT.ADS
+                Research Center – Philippines. The magazine aims to contribute
+                to human and societal development by sharing diverse articles
+                that inspire individuals to live purposefully, strive for
+                excellence, and honor the Almighty through service to others.
+            </p>
+            <br />
+            <p>
+                It features a wide range of content, including inspiring stories
+                and personal experiences of local and international
+                professionals from various fields and industries, as well as
+                expert opinions on pressing societal issues. The magazine also
+                publishes poems and essays on education, research, life, and
+                other disciplines, along with feature articles highlighting
+                educational journeys, career achievements, and professional
+                recognitions. Additionally, it showcases best practices from
+                educational institutions and other sectors, and regularly
+                highlights events organized by DIT.ADS and its partner
+                institutions.
+            </p>
+        </div>
+    );
 }
--- resources/js/Pages/Web/Magazine/Magazine.jsx
+++ resources/js/Pages/Web/Magazine/Magazine.jsx
@@ -1,5 +1,13 @@
+import MagazineImage from "../../../../../public/images/Dr.NeilsonMAGAZINE.png";
+
 export default function Magazine() {
-  return (
-    <div>Magazine</div>
-  )
+    return (
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <img
+                src={MagazineImage}
+                alt="magazine"
+                className="object-contain size-80"
+            />
+        </div>
+    );
 }
